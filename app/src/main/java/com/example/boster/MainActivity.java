af2b2377b6595f27f919644571343710c7896323
linasa <lina.b4007@gmail.com>
2022-02-23 22:40:05 +0100

    Liens entre boutons ok menu ok

--- app/src/main/java/com/example/boster/MainActivity.java
+++ app/src/main/java/com/example/boster/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.boster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +27,49 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public void onClickSaisie(View v){
+    public void onClickSaisie(){
         Intent intent = new Intent(MainActivity.this, Saisie.class);
         startActivity(intent);
-        finish();
+        //finish();
+    }
+
+    public void onClickHistorique(){
+        Intent intent = new Intent(MainActivity.this, Historique.class);
+        startActivity(intent);
+        //finish();
+    }
+
+    public void onClickSaisieB(View v){
+        Intent intent = new Intent(MainActivity.this, Saisie.class);
+        startActivity(intent);
+        //finish();
+    }
+
+    public void onClickHistoriqueB(View v){
+        Intent intent = new Intent(MainActivity.this, Historique.class);
+        startActivity(intent);
+        //finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.Accueil:
+                Toast.makeText(this, "Déjà sur la page d'accueil", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Saisir:
+                onClickSaisie();
+                break;
+            case R.id.Historique:
+                onClickHistorique();
+                break;
+            case R.id.Quitter:
+                System.exit(0);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }

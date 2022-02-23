d6208fbc840d3ece3ba7aa922d017be1835e93aa
linasa <lina.b4007@gmail.com>
2022-02-23 22:15:03 +0100

    Création des 2 autres activités

---
+++ app/src/main/java/com/example/boster/Consulter.java
@@ -0,0 +1,14 @@
+package com.example.boster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Consulter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulter);
+    }
+}

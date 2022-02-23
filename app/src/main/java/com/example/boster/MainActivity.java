ee3c19e84b2fe0c5a05379d99111e72c42886730
linasa <lina.b4007@gmail.com>
2022-02-23 22:12:10 +0100

    Clic saisie ok

--- app/src/main/java/com/example/boster/MainActivity.java
+++ app/src/main/java/com/example/boster/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.boster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void onClickSaisie(View v){
+        Intent intent = new Intent(MainActivity.this, Saisie.class);
+        startActivity(intent);
+        finish();
+    }
+
 }

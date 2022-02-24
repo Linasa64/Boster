356d2e2e8c64e746cc78cc7ff87f624b37a9042d
linasa <lina.b4007@gmail.com>
2022-02-24 10:55:10 +0100

    DB ok et page saisie ok

---
+++ app/src/main/java/com/example/boster/ClientDbHelper.java
@@ -0,0 +1,27 @@
+package com.example.boster;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ClientDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "maBD.db";
+
+    public final String SQL_CREATE = "CREATE TABLE Deplacements (id INTEGER PRIMARY KEY, mode TEXT, distance INT, ville TEXT, date DATE);";
+    final String SQL_DELETE = "DROP TABLE IF EXISTS Deplacements;";
+
+    public ClientDbHelper (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(SQL_CREATE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+}

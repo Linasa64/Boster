c111e7dd08e91459245e56ca39f07c0fca138ac5
linasa <lina.b4007@gmail.com>
2022-02-24 09:55:31 +0100

    Menu sur toutes les pages

--- app/src/main/java/com/example/boster/Consulter.java
+++ app/src/main/java/com/example/boster/Consulter.java
@@ -1,8 +1,15 @@
 package com.example.boster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class Consulter extends AppCompatActivity {
 
@@ -10,5 +17,63 @@ public class Consulter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void onClickMenu(){
+        Intent intent = new Intent(Consulter.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickSaisie(){
+        Intent intent = new Intent(Consulter.this, Saisie.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickHistorique(){
+        Intent intent = new Intent(Consulter.this, Historique.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickSaisieB(View v){
+        Intent intent = new Intent(Consulter.this, Saisie.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickHistoriqueB(View v){
+        Intent intent = new Intent(Consulter.this, Historique.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.Accueil:
+                onClickMenu();
+                break;
+            case R.id.Saisir:
+                onClickSaisie();
+                break;
+            case R.id.Historique:
+                onClickHistorique();
+                break;
+            case R.id.Quitter:
+                System.exit(0);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/boster/Saisie.java
+++ app/src/main/java/com/example/boster/Saisie.java
@@ -1,8 +1,15 @@
 package com.example.boster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class Saisie extends AppCompatActivity {
 
@@ -10,5 +17,63 @@ public class Saisie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saisie);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void onClickMenu(){
+        Intent intent = new Intent(Saisie.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickSaisie(){
+        Intent intent = new Intent(Saisie.this, Saisie.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickHistorique(){
+        Intent intent = new Intent(Saisie.this, Historique.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickSaisieB(View v){
+        Intent intent = new Intent(Saisie.this, Saisie.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickHistoriqueB(View v){
+        Intent intent = new Intent(Saisie.this, Historique.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.Accueil:
+                onClickMenu();
+                break;
+            case R.id.Saisir:
+                Toast.makeText(this, "Déjà sur la page de saisie", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Historique:
+                onClickHistorique();
+                break;
+            case R.id.Quitter:
+                System.exit(0);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

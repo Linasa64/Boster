7d780cfd590035da110631c73d814987c3d51c05
linasa <lina.b4007@gmail.com>
2022-03-21 18:15:57 +0100

    Api avec texte ok

--- app/src/main/java/com/example/boster/Pollution.java
+++ app/src/main/java/com/example/boster/Pollution.java
@@ -1,11 +1,8 @@
 package com.example.boster;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,125 +16,91 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Pollution extends AppCompatActivity {
 
-    private TextView textViewResult;
+    private TextView textViewPollution;
+    private TextView textViewNomVille;
+    private TextView textViewCommentaire;
+
+    String idStation;
+
     JsonPlaceHolderApi jsonPlaceHolderApi;
 
-    //Association entre le nom de la station et son identifiant sur le site internet
-    Station toulMaz = new Station("Toulouse Mazades", "@3827");
-    Station toulBer = new Station("Toulouse Berthelot", "@3828");
-    Station toulRAlbi = new Station("Toulouse-Rte D'Albi", "@3829");
-    Station blagPiste = new Station("Blagnac Aéroport Côté Pistes", "@3830");
-    Station lourdes = new Station("Lourdes Lapacca", "@3831");
-    Station toulZIChap = new Station("Toulouse ZI Chapitre", "@3832");
-    Station toulEizen = new Station("Toulouse Eisenhower", "@3833");
-    Station albiDel = new Station("Albi Sq. Delmas", "@3834");
-    Station blagTraf = new Station("Blagnac Aéroport Côté Trafic", "@3835");
-    Station toulJacqu = new Station("Toulouse Jacquier", "@3836");
-    Station tarbes = new Station("Tarbes J. Dupuy", "@3837");
-    Station montau = new Station("Montauban Farguettes", "@8603");
-    Station peyr = new Station("Peyrusse-Vieille", "@3838");
-    Station pauTourasse = new Station("Tourasse, Pau, Aquitaine", "@5069");
-
-    private Station[] stations = {toulMaz, toulBer, toulRAlbi, blagPiste, lourdes, toulZIChap, toulEizen, albiDel, blagTraf, toulJacqu, tarbes, montau, peyr, pauTourasse};
-    int nbStations = stations.length;
-
-    private String[] nomsStations = new String[nbStations];
-
-    AutoCompleteTextView choixVille;
-    Button validerB;
-
-    String idSelected;
-
     float pollution;
+    String nomVille;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pollution);
 
-
-        for(int i = 0 ; i<nbStations ; i++){
-            nomsStations[i] = stations[i].getNom();
-        }
-
-        choixVille = (AutoCompleteTextView) findViewById(R.id.choixVille);
-        validerB = (Button) findViewById(R.id.valider);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_item, nomsStations);
-
-        AutoCompleteTextView actv =  (AutoCompleteTextView)findViewById(R.id.choixVille);
-        actv.setThreshold(1);
-        actv.setAdapter(adapter);
-
-        textViewResult = findViewById(R.id.results);
-
         Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://api.waqi.info/")
+                .baseUrl("https://api.waqi.info/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
+        textViewPollution = findViewById(R.id.results);
+        textViewNomVille = findViewById(R.id.nomVilleSelectionnee);
+        textViewCommentaire = findViewById(R.id.commentairePollution);
+    }
+
+    public void onClickValider(View v){
+
+        Intent intent = new Intent(Pollution.this, ChoixVilleApi.class);
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==2 && resultCode == RESULT_OK){
+            if(data != null){
+                idStation = ""+ data.getStringExtra("idStation");
+                nomVille = "" + data.getStringExtra("nomStation");
+                Toast.makeText(this, "caca : "+data.getStringExtra("nomStation"), Toast.LENGTH_SHORT).show();
+                getData();
+            }
+        }
     }
 
     private void getData(){
 
-        Call<APIResponse> call = jsonPlaceHolderApi.data(idSelected);
+        Call<APIResponse> call = jsonPlaceHolderApi.data(idStation);
 
         call.enqueue(new Callback<APIResponse>() {
             @Override
             public void onResponse(Call<APIResponse> call, Response<APIResponse> response) {
                 if(!response.isSuccessful()){
-                    textViewResult.setText(("Error response :" + response.code()));
+                    textViewPollution.setText(("Error response :" + response.code()));
                     return;
                 }
 
                 APIResponse data = response.body();
                 String content = "";
                 content += data.data().aqi();
-                pollution = Float.parseFloat(content);
-                textViewResult.append(content);
+                pollution = data.data().aqi();
+                textViewPollution.setText("Niveau de pollution :  " + content + "  aqi");
+                textViewNomVille.setText(nomVille);
                 content ="";
+                nomVille="";
+
+                if(pollution<50){
+                    textViewCommentaire.setText("La qualité de l'air est bonne");
+                }
+                else if(pollution<100){
+                    textViewCommentaire.setText("La qualité de l'air est modérément bonne");
+                }
+                else if(pollution<150){
+                    textViewCommentaire.setText("La qualité de l'air est mauvaise pour les personnes sensibles");
+                }
             }
 
             @Override
             public void onFailure(Call<APIResponse> call, Throwable t) {
-                textViewResult.setText((t.getMessage()));
+                //textViewResult.setText((t.getMessage()));
             }
         });
-    }
-
-    public void onClickValider(View v){
-
-        for(int i = 0 ; i<nbStations ; i++){
-            if(stations[i].getNom().equals(choixVille.getText().toString())){
-                idSelected = stations[i].getId();
-            }
-        }
-
-        getData();
-
-        if(pollution <50.0){
-            textViewResult.setBackgroundColor(Color.GREEN);
-            Toast.makeText(this, "Vert", Toast.LENGTH_SHORT).show();
-        }
-        else if(pollution<100.0){
-            textViewResult.setBackgroundColor(Color.YELLOW);
-            Toast.makeText(this, "Jaune", Toast.LENGTH_SHORT).show();
-        }
-        else if(pollution <150){
-            textViewResult.setBackgroundColor(Color.parseColor("#f39c12"));
-        }
-        else if(pollution <100){
-            textViewResult.setBackgroundColor(Color.RED);
-        }
-        else if(pollution <100){
-            textViewResult.setBackgroundColor(Color.parseColor(" #8e44ad "));
-        }
-        else if(pollution <100){
-            textViewResult.setBackgroundColor(Color.parseColor("#641e16"));
-        }
-
     }
 
 }
